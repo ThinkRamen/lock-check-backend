073faf50dff3226e648af334f88d4ad24620d9df
Alejandro <ThinkRamen@gmail.com>
2023-01-15 00:30:58 -0800

    added api app and customer model

--- django_backend/settings.py
+++ django_backend/settings.py
@@ -31,6 +31,11 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    # local
+    'apis.apps.ApisConfig',
+    # third party
+    'rest_framework',   
+    # included
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
